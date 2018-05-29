274091ca06755a8a382bab59c9accb79a347fbd1
backflipper1991 <sashaulych@gmail.com>
2018-05-29 19:23:51 +0300

    materialize react added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react';
+import { Button, Card, Row, Col, Collapsible, CollapsibleItem } from 'react-materialize';
+// import $ from 'jquery'; 
 import './App.css';
+
 const listArr = [
   {name: "John", id: 120, icon: "filter_drama", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut la"},
   {name: "Beth", id: 443, icon: "place", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut "},
   {name: "Jane", id: 510, icon: "whatshot", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit"}
  ];
 
-function InputItem(props) {
-  return <input type="text" placeholder="Enter new item" value="" />; 
+class InputItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    console.log('test')
+  }
+  render() {
+    return (
+    <input type="text" placeholder="Enter new item" value={this.state.value} onSubmit={this.handleSubmit} />
+    ); 
+  }
 }
 class renderList extends Component {
   render() {
     return (
       <div className="container">
         <InputItem />
-        <ul className="collapsible">
-            {
-              listArr.map(function(user, i){
-                return <li key={i}>
-                  <div className="collapsible-header"><i className="material-icons">{user.icon}</i>{user.name}</div>
-                  <div className="collapsible-body"><span>{user.text}</span></div>
-                </li>;
-              })
-            }
-        </ul>
+        <Collapsible>
+        {
+          listArr.map(function(user, i){
+            var status = (i === 0) ? 'active' : ''; 
+            return <CollapsibleItem key={i} className={status} header={user.name} icon={user.icon}>
+              {user.text}
+            </CollapsibleItem>;
+          })
+        }
+        </Collapsible>
       </div>
     );
   }
